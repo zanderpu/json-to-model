6f2eb81a4ac1bf9972a0554c56092298278dd03f
chenzehao <czh123456>
2020-11-09 19:08:13 +0800

    init project

---
+++ src/components/Decode.ts
@@ -0,0 +1,182 @@
+const baseModelTemplete:string = `
+import 'package:json_annotation/json_annotation.dart';
+part 'index_data.g.dart'; 
+
+@JsonSerializable(explicitToJson: true)
+class TempleteModel {
+    TempleteData data;
+
+    TempleteModel({
+      this.data,
+      this.code,
+      this.message
+    });
+	
+    factory TempleteModel.fromJson(Map<String, dynamic> json) => _$TempleteModelFromJson(json);
+    Map<String, dynamic> toJson() => _$TempleteModelToJson(this);
+}
+`;
+const baseModelTempleteData:string = `
+@JsonSerializable(explicitToJson: true)
+class TempleteData {
+    ContentToDo
+    TempleteData({
+    thisInit
+    });
+
+  factory TempleteData.fromJson(Map<String, dynamic> json) => _$TempleteDataFromJson(json);
+  Map<String, dynamic> toJson() => _$TempleteDataToJson(this);
+}
+
+`;
+const baseModelBottomToJson:string = `
+
+    factory TempleteData.fromJson(Map<String, dynamic> json) => _$TempleteDataFromJson(json);
+    Map<String, dynamic> toJson() => _$TempleteDataToJson(this);
+`;
+
+enum BaseDataType  {
+  String =   "[object String]",
+  Number = "[object Number]",
+  Boolean = "[object Boolean]",
+  Object ="[object Object]",
+  Array ="[object Array]",
+  Null = "[object Null]",
+}
+
+let fileName:string = "";
+let chreateChildTemps:string[] = [];
+
+function isFloat(val:number):boolean{
+  if(val === 1){
+    return false
+  }
+  return val % 1 != 0;
+}
+
+function getThisInit(obj:object):string {
+    let initStr:string = "";
+    for (let key in obj) {
+      initStr = initStr + `    this.${key}, \n    `;
+    }
+    return initStr;
+}
+  
+function getBottomToJson(str:string):string {
+  return baseModelBottomToJson.replace(/TempleteData/g, str);
+}
+
+function judgeType(val:any):string {
+  return Object.prototype.toString.call(val);
+}
+
+function firstUpperCase(str:string):string {
+  let val:string = "";
+  str
+    .split("_")
+    .map(
+      (item) =>
+        (val =
+          val +
+          item.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase()))
+    );
+  return val;
+}
+
+
+function createChildData(className:string, obj:object) {
+  let value =`@JsonSerializable(explicitToJson: true) \nclass ${className}{ \n    ${getDataType(
+    obj
+  )}\n    ${className}({\n    ${getThisInit(obj)}});${getBottomToJson(className)} \n}\n\n `;
+  chreateChildTemps.push(value);
+}
+
+function getDataType(obj:object):string {
+  let result = "";
+  for (let key in obj) {
+    switch (judgeType(obj[key])) {
+      case BaseDataType.String:
+        result = `${result}String ${key};\n    `;
+        break;
+
+      case BaseDataType.Number:
+        result = `${result}${isFloat(obj[key]) ? 'double': 'int'} ${key};\n    `;
+        break;
+
+      case BaseDataType.Boolean:
+        result = `${result}bool ${key};\n    `;
+        break;
+
+      case BaseDataType.Null:
+        result = `${result}var ${key};\n    `;
+        break;
+
+      case BaseDataType.Object:
+        result = `${result}${firstUpperCase(key)}Data ${key};\n    `;
+        createChildData(`${firstUpperCase(key)}Data`, obj[key]);
+        break;
+        
+      case BaseDataType.Array:
+        if (obj[key].length > 0) {
+          switch (judgeType(obj[key][0])) {
+            case BaseDataType.Number:
+              result = `${result}List<${isFloat(obj[key][0]) ? 'double': 'int'}> ${key}; \n    `;
+              break;
+
+            case BaseDataType.String:
+              result = `${result}List<String> ${key}; \n    `;
+              break;
+
+            case BaseDataType.Object:
+              result = `${result}List<${firstUpperCase(key)}Data> ${key}; \n    `;
+              createChildData(`${firstUpperCase(key)}Data`, obj[key][0]);
+              break;
+
+            case BaseDataType.Array:
+              result = `${result}List<${firstUpperCase(key)}Data> ${key}; \n    `;
+              createChildData(`${firstUpperCase(key)}Data`, obj[key][0]);
+              break;
+          }
+        } else {
+          result = `${result}List<dynamic> ${key}; \n    `;
+        }
+        break;
+    }
+  }
+
+  return result;
+}
+
+
+export default function decodeObj(value, filename:string) {
+  fileName = filename;
+  chreateChildTemps = [];
+  let dataResultString:string = `${firstUpperCase(fileName)}Model`;
+  let dataTypeString:string;
+  let isArrayValue:boolean = false;
+  if (value.data) {
+    value = value.data;
+  }
+  if (Array.isArray(value)) {
+    isArrayValue = true;
+    value = value[0];
+  }
+  dataTypeString = isArrayValue
+    ? `List<${firstUpperCase(fileName)}Data>`
+    : `${firstUpperCase(fileName)}Data`;
+  let tempResult = baseModelTemplete
+    .replace(/TempleteModel/g, dataResultString)
+    .replace(/TempleteData/, dataTypeString)
+    .replace(/index/, fileName);
+  let dataParentType:string = getDataType(value);
+  let childTempValue:string = "";
+  dataParentType = baseModelTempleteData
+    .replace(/TempleteData/g, `${firstUpperCase(fileName)}Data`)
+    .replace(/ContentToDo/, dataParentType)
+    .replace(/thisInit/, getThisInit(value));
+
+  chreateChildTemps.reverse().map((item) => (childTempValue = `${childTempValue}${item}`));
+
+  return `${tempResult}${dataParentType}${childTempValue}`;
+}
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,156 @@
+<template>
+  <div>
+    <div class="tip">
+      <h1>JSON to Model</h1>
+      <h4>Paste your JSON in the textarea below, click convert and get your Dart classes for free.</h4>
+    </div>
+    <div class="flex file_name">
+      <h4 sytle="flex-shrink:0">ClassName:</h4>
+      <el-input
+        class="file_name--input flex1"
+        v-model="input"
+        placeholder="Input Class Name"
+      />
+    </div>
+    <div class="flex">
+      <el-input
+        type="textarea"
+        placeholder="Input JSON Content"
+        :autosize="{ minRows: 30, maxRows: 30 }"
+        v-model="inputData"
+      >
+      </el-input>
+      <div>
+        <div class="translate_btn" @click="onTrans">
+          <el-button type="primary"
+            >Go<i
+              :class="[
+                'el-icon-arrow-right',
+                isTrans ? 'el-icon-loading' : 'el-icon--right',
+              ]"
+            ></i
+          ></el-button>
+        </div>
+        <div class="translate_btn" @click="copy">
+          <el-button type="success"
+            >Get<i class="el-icon-document-copy el-icon--right"></i
+          ></el-button>
+        </div>
+      </div>
+      <el-input
+        id="output"
+        type="textarea"
+        :autosize="{ minRows: 30, maxRows: 30 }"
+        v-model="outputData"
+        class="output"
+        readonly
+      >
+      </el-input>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import decode from "./Decode.ts";
+
+export default {
+  name: "HelloWorld1s",
+  data() {
+    return {
+      input: "",
+      inputData: "",
+      outputData: "",
+      isTrans: false,
+    };
+  },
+
+  mounted() {
+  },
+  methods: {
+    onTrans() {
+      if (this.inputData.trim().length == 0) {
+        return this.$message({
+          message: "JSON Can't Empty",
+          type: "error",
+        });
+      }
+      this.isTrans = true;
+      try {
+        let value = JSON.parse(JSON.parse(JSON.stringify(this.inputData)));
+        if (typeof value != "object") {
+          return this.$message({
+            message: "Class Name Can't Empty",
+            type: "error",
+          });
+        }
+        this.outputData = decode(value, this.input.trim().length == 0 ? 'auto' : this.input);
+      } catch (err) {
+        return this.$message({
+          message: "JSON Is Not True",
+          type: "error",
+        });
+      } finally {
+        this.isTrans = false;
+      }
+    },
+    copy() {
+      if (this.outputData.trim().length == 0) {
+        return this.$message({
+          message: "Model Is Empty",
+          type: "error",
+        });
+      }
+      let copyText = document.getElementById("output");
+      copyText.select();
+      document.execCommand("copy");
+      this.$message({
+        message: "Copy Success",
+        type: "success",
+      });
+    },
+    getQueryString(name) {
+      let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+      let r = window.location.search.substr(1).match(reg);
+      if (r != null) return decodeURI(r[2]);
+      return '';
+    },
+  },
+};
+</script>
+<style>
+.flex {
+  display: flex;
+  align-items: center;
+}
+.flex1 {
+  flex: 1;
+}
+.translate_btn {
+  margin: 20px;
+}
+.file_name {
+  margin-bottom: 30px;
+  width: 400px;
+}
+.file_name--input {
+  margin: 0 10px;
+}
+.el-textarea__inner {
+  resize: none;
+}
+.el-textarea__inner{
+  color: #2b2b2b!important;
+  resize: none!important;
+  outline: none!important;
+  box-shadow: none !important;
+}
+.output .el-textarea__inner{
+  border:node !important;
+}
+
+.tip{
+  margin-bottom: 30px;
+}
+
+</style>
